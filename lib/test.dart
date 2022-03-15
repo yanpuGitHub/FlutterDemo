099e9e5e5b1267845bcb6592bc67105838ec2881
yp <yanpu@yxj.org.cn>
2022-03-15 15:32:20 +0800

    feature: 搭建项目

---
+++ lib/test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+
+typedef inA = bool Function();
+
+String a = "";
+
+bool isNoble() => true;
+
+void main() => runApp(const MyPage());
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class Homepage extends StatefulWidget{
+  @override
+  State<Homepage> createState()  => _MyHomePageState();
+
+}
+
+class MyHomePage extends StatefulWidget{
+
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<Homepage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
