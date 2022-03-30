e2e7eae02b86e0183fcb758be54e133d6e6ba49b
yp <yanpu@yxj.org.cn>
2022-03-30 14:23:28 +0800

    Revert "Feature: 测试git回退版本2"
    This reverts commit 91077842

--- lib/test.dart
+++ lib/test.dart
@@ -3,11 +3,11 @@ import 'package:flutter/cupertino.dart';
 typedef inA = bool Function();
 
 String a = "";
-/// 测试git回退版本1 回退合并的分支代码1
+/// 测试git回退版本1
 bool isNoble() => true;
-/// 测试git回退版本2 回退合并的分支代码2
+
 void main() => runApp(const MyPage());
-/// 测试git回退版本3
+
 class MyPage extends StatelessWidget {
   const MyPage({Key? key}) : super(key: key);
 
